fa220e7a097ff5d413006e5fd76704d37fa3f6c6
KeiichiNaritomi <104675165+naritomi55@users.noreply.github.com>
2022-05-12 10:20:00 +0000

    [Finish]4-6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permittede_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permittede_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
 end
